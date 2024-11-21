586b561443ae1ad6f9205b20747f568a90e8ca38
JoseCarlos-04 <jparrom736@iescarrillo.es>
2024-11-21 01:39:08 +0100

    Clase 20/11/2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,25 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { TasklistComponent } from "./components/task/tasklist/tasklist.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, TasklistComponent, FooterComponent],
+  imports: [NavbarComponent, TasklistComponent, FooterComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  imagenAleatoria:string = "";
+  nombre: string = "";
+  numero: number = 0;
+
+  cargarImagenAleatoria(){
+    this.imagenAleatoria = "https://picsum.photos/200/300?random=" + this.randomInt();
+  }
+
   randomInt(): number{
-    return Math.random()*10;
+    return Math.trunc((Math.random()*200) + 1);
   }
 }
